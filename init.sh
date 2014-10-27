ddfc3b19d4b1b94285cc95a11760ce066af24da1
kain <me@kain-jy.com>
2014-10-27 10:36:07 +0900

    Add initialize script

---
+++ init.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/kain-jy/dotfiles $HOME/.dotfiles
+
+mkdir -p $HOME/.vim/bundle
+mkdir -p $HOME/.vim/rc.d
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
