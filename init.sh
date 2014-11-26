19fc0e9b0c5bde84e938f26a90a91b950b68e792
kain <me@kain-jy.com>
2014-11-27 00:45:53 +0900

    Fixed to add create after/ftplugin directory

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/kain-jy/dotfiles $HOME/.dotfiles
 
 mkdir -p $HOME/.vim/bundle
+mkdir -p $HOME/.vim/after/ftplugi
 mkdir -p $HOME/.vim/rc.d
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
